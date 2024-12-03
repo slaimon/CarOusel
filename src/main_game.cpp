0e43f30a329a355a9f19f9b5a182400577651bf2
Slaimon <simo.bertolucci@gmail.com>
2024-12-03 14:04:38 +0100

    Adapt viewport to window size

--- src/main_game.cpp
+++ src/main_game.cpp
@@ -643,6 +643,10 @@ int main(int argc, char** argv) {
 
    glEnable(GL_DEPTH_TEST);
    while (!glfwWindowShouldClose(window)) {
+      // in case the window was resized
+      glfwGetWindowSize(window, &width, &height);
+      glViewport(0, 0, width, height);
+
       glClearColor(0.3f, 0.3f, 0.3f, 1.f);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
       check_gl_errors(__LINE__, __FILE__);
